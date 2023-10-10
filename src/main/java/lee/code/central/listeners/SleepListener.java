6f8ec6862b90223ee8a623c7243e4e75b64f85b6
Lee <leecook808@gmail.com>
2023-10-10 10:26:40 -0700

    fix

--- src/main/java/lee/code/central/listeners/SleepListener.java
+++ src/main/java/lee/code/central/listeners/SleepListener.java
@@ -7,6 +7,7 @@ import org.bukkit.block.data.type.Bed;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerBedEnterEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -23,6 +24,7 @@ public class SleepListener implements Listener {
     if (!e.hasBlock()) return;
     if (e.getClickedBlock() == null) return;
     if (!(e.getClickedBlock().getBlockData() instanceof Bed)) return;
+    if (!e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) return;
     final Player player = e.getPlayer();
     if (!(player.getWorld().getEnvironment().equals(World.Environment.NORMAL))) return;
     e.setCancelled(true);
